445a63ef5efbdeb381dc5f678710a711cb32cfc8
Shane Ferrell <shaneferrellwv@gmail.com>
2024-04-18 15:07:44 -0400

    fix alignment on pet update page

--- update.php
+++ update.php
@@ -24,55 +24,57 @@
 <body>
     <?php include('theme/header.php'); ?>
     <div class='container-fluid'>
-        <h2>Update Pet</h2>
-        <div class="card">
-            <div class="card-body">
+        <div class="container mt-5">
+            <h2>Update Pet</h2>
+            <div class="card">
+                <div class="card-body">
 
-                <?php
-                    $pet = selectPet($_GET['id']);
-                ?>
+                    <?php
+                        $pet = selectPet($_GET['id']);
+                    ?>
 
-                <form action="" method="post" class="form" enctype="multipart/form-data">
-                <div class="row">
-                    <div class="col-md-6">
-                        <label for="name">Name</label>
-                        <input type="text" name="name" id="name" class="form-control" value="<?php echo $pet['name']; ?>">
-                        <br>
+                    <form action="" method="post" class="form" enctype="multipart/form-data">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name" class="form-control" value="<?php echo $pet['name']; ?>">
+                            <br>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="age">Age</label>
+                            <input type="number" name="age" id="age" class="form-control" value="<?php echo $pet['age']; ?>" min="0">
+                            <br>
+                        </div>
                     </div>
-                    <div class="col-md-6">
-                        <label for="age">Age</label>
-                        <input type="number" name="age" id="age" class="form-control" value="<?php echo $pet['age']; ?>" min="0">
-                        <br>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="species">Species</label>
+                            <select name="species" id="species" class="form-control">
+                                <option value="dog" <?php echo ($pet['species'] == 'dog') ? 'selected' : ''; ?>>dog</option>
+                                <option value="cat" <?php echo ($pet['species'] == 'cat') ? 'selected' : ''; ?>>cat</option>
+                                <option value="bird" <?php echo ($pet['species'] == 'bird') ? 'selected' : ''; ?>>bird</option>
+                            </select>
+                            <br>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="breed">Breed</label>
+                            <input type="text" name="breed" id="breed" class="form-control" value="<?php echo $pet['breed']; ?>">
+                            <br>
+                        </div>
                     </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <label for="description">Description</label>
+                            <textarea name="description" id="description" class="form-control"><?php echo $pet['description']; ?></textarea>
+                            <br>
+                        </div>
+                    </div>
+                    <button name="btnUpdate" class="btn btn-primary">Update Pet</button>
+                    <button name="btnDelete" class="btn btn-danger">Delete Pet</button>
+                    </form>  
                 </div>
-                <div class="row">
-                    <div class="col-md-6">
-                        <label for="species">Species</label>
-                        <select name="species" id="species" class="form-control">
-                            <option value="dog" <?php echo ($pet['species'] == 'dog') ? 'selected' : ''; ?>>dog</option>
-                            <option value="cat" <?php echo ($pet['species'] == 'cat') ? 'selected' : ''; ?>>cat</option>
-                            <option value="bird" <?php echo ($pet['species'] == 'bird') ? 'selected' : ''; ?>>bird</option>
-                        </select>
-                        <br>
-                    </div>
-                    <div class="col-md-6">
-                        <label for="breed">Breed</label>
-                        <input type="text" name="breed" id="breed" class="form-control" value="<?php echo $pet['breed']; ?>">
-                        <br>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <label for="description">Description</label>
-                        <textarea name="description" id="description" class="form-control"><?php echo $pet['description']; ?></textarea>
-                        <br>
-                    </div>
-                </div>
-                <button name="btnUpdate" class="btn btn-primary">Update Pet</button>
-                <button name="btnDelete" class="btn btn-danger">Delete Pet</button>
-                </form>  
             </div>
-        </div>
+        </div>      
     </div>
     <br>
     <?php include('theme/footer.php'); ?>
