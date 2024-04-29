3a6a5432966505ff8915b3f3e9d1cd0d7e40678f
Shane Ferrell <shaneferrellwv@gmail.com>
2024-04-29 12:56:29 -0400

    fix homepage and add carousel of adoptable pets

--- includes/functions.php
+++ includes/functions.php
@@ -119,6 +119,23 @@ function deletePet($petid){
     exit();
 }
 
+function getPetCarousel() {
+    global $mysqli;
+    $data = array();
+    $stmt = $mysqli->prepare('SELECT name, description, image_url FROM pets');
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $pets = [];
+    while ($row = $result->fetch_assoc()) {
+        $pets[] = [
+            'image_url' => $row['image_url'],
+            'name' => $row['name'],
+            'description' => $row['description']
+        ];
+    }
+    return $pets;
+}
+
 /* ====================== APPLICATIONS ==================== */
 
 /* insert */
